93a0735fc94c5c3cddc83847da33858e12b6c4d5
Douglas Smolareki Vieira <douglas.vieira@ezcommerce.local>
2019-03-19 13:15:10 -0300

    Add project files.

---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Alex75;
+using System.Net;
+using System.IO;
+using Jayrock;
+
+/// Classe para formatar Json
+
+class JsonHelper
+{
+    private const string INDENT_STRING = "    ";
+    public static string FormatJson(string str)
+    {
+        var indent = 0;
+        var quoted = false;
+        var sb = new StringBuilder();
+        for (var i = 0; i < str.Length; i++)
+        {
+            var ch = str[i];
+            switch (ch)
+            {
+                case '{':
+                case '[':
+                    sb.Append(ch);
+                    if (!quoted)
+                    {
+                        sb.AppendLine();
+                        Enumerable.Range(0, ++indent).ForEach(item => sb.Append(INDENT_STRING));
+                    }
+                    break;
+                case '}':
+                case ']':
+                    if (!quoted)
+                    {
+                        sb.AppendLine();
+                        Enumerable.Range(0, --indent).ForEach(item => sb.Append(INDENT_STRING));
+                    }
+                    sb.Append(ch);
+                    break;
+                case '"':
+                    sb.Append(ch);
+                    bool escaped = false;
+                    var index = i;
+                    while (index > 0 && str[--index] == '\\')
+                        escaped = !escaped;
+                    if (!escaped)
+                        quoted = !quoted;
+                    break;
+                case ',':
+                    sb.Append(ch);
+                    if (!quoted)
+                    {
+                        sb.AppendLine();
+                        Enumerable.Range(0, indent).ForEach(item => sb.Append(INDENT_STRING));
+                    }
+                    break;
+                case ':':
+                    sb.Append(ch);
+                    if (!quoted)
+                        sb.Append(" ");
+                    break;
+                default:
+                    sb.Append(ch);
+                    break;
+            }
+        }
+        return sb.ToString();
+    }
+}
+
+static class Extensions
+{
+    public static void ForEach<T>(this IEnumerable<T> ie, Action<T> action)
+    {
+        foreach (var i in ie)
+        {
+            action(i);
+        }
+    }
+}
+
+/////////// END  //////////////////
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string html = string.Empty;
+            string url = @"http://www.reference.corecommerce.com.br/pesquisa.json";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            Console.WriteLine(html);
+            
+            this.richTextBox1.AppendText(JsonHelper.FormatJson(html));
+            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
